91f867d8535df131de676055d1e75f2b9436f8b3
yli <yli@cs.hku.hk>
2015-06-02 15:37:24 +0800

    Multiple Datasource Implemented Successfully.

--- src/main/java/org/lyndon/config/BaseJpaConfig.java
+++ src/main/java/org/lyndon/config/BaseJpaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import java.util.Properties;
@@ -13,6 +14,7 @@ import java.util.Properties;
  * Created by yli on 6/2/2015.
  */
 @Configuration
+@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 public class BaseJpaConfig {
 
--- src/main/java/org/lyndon/config/SpringJpaConfig.java
+++ src/main/java/org/lyndon/config/SpringJpaConfig.java
@@ -6,18 +6,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * Created by yli on 6/2/2015.
@@ -25,8 +19,6 @@ import java.util.Properties;
 @Configuration
 @EnableJpaRepositories(basePackages = {"org.lyndon.repository1"}, entityManagerFactoryRef = "springEntityManagerFactory",
         transactionManagerRef = "springTransactionManager")
-@EnableTransactionManagement
-@PropertySource("classpath:application.properties")
 public class SpringJpaConfig extends BaseJpaConfig {
 
     private static final String DATABASE_DRIVER = "db.driver";
